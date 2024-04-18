0c67fbb3158a933094466ecd81365c9a8d34df27
Mcgroupfilm <130358785+Mcgroupfilm@users.noreply.github.com>
2024-04-18 14:39:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 
 API_HASH     = "27bd653cd164958dbfe7c942546e018d"
-BOT_TOKEN    = "6607359975:AAEW9mrCkQpxkab7wHibBRCb1EgKkBdiXJ8"
+BOT_TOKEN    = "5907820266:AAHKnmZjoAEytyNQb3LiyQewfwsar1CmpwI"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://TVK:TVK@cluster0.rxjrujo.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
